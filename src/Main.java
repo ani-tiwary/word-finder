e4083df6d38d0955fe63dc391863dc1b373e2a59
ani tiwary <137713147+ani-tiwary@users.noreply.github.com>
2023-07-17 15:44:44 -0400

    Add files via upload
    added multithreading

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,45 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 public class Main {
-    private static void permute(String prefix, String suffix, List<String> rearrangements) throws FileNotFoundException{
-        VerifyWord verifier = new VerifyWord();
+    private static void permute(String prefix, String suffix, List<String> rearrangements, VerifyWord verifier) throws FileNotFoundException {
         int length = suffix.length();
-        if(verifier.isWord(prefix)) {
+        if (verifier.isWord(prefix)) {
             rearrangements.add(prefix);
         }
-        for (int i = 0; i < length; i++) {
-            String newPrefix = prefix + suffix.charAt(i);
-            String newSuffix = suffix.substring(0, i) + suffix.substring(i + 1);
-            permute(newPrefix, newSuffix, rearrangements);
+        if (length > 1) {
+            List<Thread> threads = new ArrayList<>();
+            for (int i = 0; i < length; i++) {
+                String newPrefix = prefix + suffix.charAt(i);
+                String newSuffix = suffix.substring(0, i) + suffix.substring(i + 1);
+                Thread thread = new Thread(() -> {
+                    try {
+                        permute(newPrefix, newSuffix, rearrangements, verifier);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
+                threads.add(thread);
+                thread.start();
+            }
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
-    public static ArrayList<String> genRearrangements(String input) throws FileNotFoundException{
+    public static ArrayList<String> genRearrangements(String input, int numThreads) throws FileNotFoundException {
         ArrayList<String> rearrangements = new ArrayList<>();
-        permute("", input, rearrangements);
+        VerifyWord verifier = new VerifyWord();
+        permute("", input, rearrangements, verifier);
         return rearrangements;
     }
-    public static void main(String[] args) throws FileNotFoundException{
-        String input = "abcd";
-        ArrayList<String> rearrangements = genRearrangements(input);
+    public static void main(String[] args) throws FileNotFoundException {
+        String input = "abcdef";
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        ArrayList<String> rearrangements = genRearrangements(input, numThreads);
         System.out.println(rearrangements);
     }
 }
